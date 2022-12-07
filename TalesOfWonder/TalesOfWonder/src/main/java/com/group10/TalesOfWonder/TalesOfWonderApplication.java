0fc7945556fe30fc09dd66b93e019692f45855ef
trungluong12 <51406911+trungluong12@users.noreply.github.com>
2022-12-07 23:33:39 +0700

    Add files via upload

---
+++ TalesOfWonder/TalesOfWonder/src/main/java/com/group10/TalesOfWonder/TalesOfWonderApplication.java
@@ -0,0 +1,13 @@
+package com.group10.TalesOfWonder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalesOfWonderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TalesOfWonderApplication.class, args);
+	}
+
+}
