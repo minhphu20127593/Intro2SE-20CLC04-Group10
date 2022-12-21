9194fa0bdeb1f24f00867811d46aa6c7d687a357
Tuanquan127 <ltquan20@clc.fitus.edu.vn>
2022-12-21 08:38:24 +0700

    migrate path of file

--- TalesOfWonder/TalesOfWonder/src/main/java/com/group10/TalesOfWonder/TalesOfWonderApplication.java
+++ TalesOfWonder/TalesOfWonder/src/main/java/com/group10/TalesOfWonder/TalesOfWonderApplication.java
@@ -2,12 +2,14 @@ package com.group10.TalesOfWonder;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+
 
 @SpringBootApplication
+@RestController
 public class TalesOfWonderApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TalesOfWonderApplication.class, args);
 	}
-
 }
