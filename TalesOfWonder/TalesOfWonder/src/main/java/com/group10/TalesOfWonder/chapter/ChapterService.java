d21947468debe3b3d35b988314d47250d57308d3
minhphu20127593 <laiphu86@gmail.com>
2023-01-03 10:29:35 +0700

    Merge branch 'main' of https://github.com/minhphu20127593/Intro2SE-20CLC04-Group10

--- TalesOfWonder/TalesOfWonder/src/main/java/com/group10/TalesOfWonder/chapter/ChapterService.java
+++ TalesOfWonder/TalesOfWonder/src/main/java/com/group10/TalesOfWonder/chapter/ChapterService.java
@@ -2,7 +2,12 @@ package com.group10.TalesOfWonder.chapter;
 
 import com.group10.TalesOfWonder.entity.Chapter;
 import com.group10.TalesOfWonder.entity.Comic;
+import com.group10.TalesOfWonder.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.*;
@@ -25,6 +30,9 @@ public class ChapterService {
 
         return chapterRepository.save(chapter);
     }
+    public Chapter findChapterByID(int id) {
+        return chapterRepository.findById(id).get();
+    }
     public List<Chapter> getListChapterOfAComic(Comic comic) {
         return chapterRepository.findByComicID(comic.getId());
     }
@@ -34,6 +42,17 @@ public class ChapterService {
             List<Chapter> chapterComic = chapterRepository.findByComicIDLimit(comic.getId(),3);
             chapters.put(comic.getId(),chapterComic);
         }
+        return chapters;
+    }
+
+    public Page<Chapter> listByPageOfChapter(int pageNum, String sortField, String sortDir, String keyword, Comic comic) {
+        Sort sort = Sort.by(sortField);
+        sort = sortDir.equals("asc") ? sort.ascending() : sort.descending();
+        Pageable pageable = PageRequest.of(pageNum - 1,10,sort);
+        if (keyword != null)
+            return chapterRepository.findAllByKeyWord(keyword,comic,pageable);
+        Page<Chapter> chapters = chapterRepository.findAllByComic(comic.getId(),pageable);
+
         return chapters;
     }
 }
